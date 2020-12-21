52735e9feff42c0e22c4085164c9bfd62ac4b80d
KosmosBro <76169732+KosmosBro@users.noreply.github.com>
2020-12-21 18:26:32 +0600

    Add files via upload

---
+++ month_spisok.py
@@ -0,0 +1,19 @@
+
+month = input('Введите месяц : ')
+
+winter = ["декабрь", "январь", "февраль"]
+spring = ["март", "апрель", "май"]
+summer = ["июнь", "июль", "июль"]
+autumn = ["сентябрь", "октябрь", "ноябрь"]
+
+if month in winter:
+    print('Сейчас зима, холодно!')
+
+elif month in autumn:
+    print('Сейчас осень, прохладно!')
+
+elif month in spring:
+    print('Сейчас весна, тепло!')
+
+elif month in summer:
+    print('Сейчас лето, жарко!')
