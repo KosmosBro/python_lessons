ef1fe90d58318329afe2ae9d16221225942c2f98
KosmosBro <76169732+KosmosBro@users.noreply.github.com>
2020-12-23 15:30:42 +0600

    Add files via upload

---
+++ lst_cycle.py
@@ -0,0 +1,24 @@
+      #ЗАДАНИЕ № 1
+
+i = 0
+
+while i in range(0, 100):
+    print(i)
+    i += 7
+
+
+      #  ЗАДАНИЕ № 2
+
+# male = []
+# female = list()
+# for i in range(1, 5):
+#     pol = input('Укажите ваш пол : ')
+#     name = input('Укажите имя : ')
+#
+#     if 'женский' in pol:
+#         male.append(name)
+#     elif "мужской" in pol:
+#         female.append(name)
+#     else:
+#         print("Введите корректное значение")
+# print(male, female)
