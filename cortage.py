ab4a7adad4c02b18f533fa16d69ea3abc3b2d952
KosmosBro <76169732+KosmosBro@users.noreply.github.com>
2020-12-25 19:31:42 +0600

    Update cortage.py

--- cortage.py
+++ cortage.py
@@ -1,20 +1,18 @@
+car = list()
+for i in range(1, 4):
+    brand_car = input("Введите бренд авто : ")
+    model_car = input("Введите модель авто : ")
+    year_car = input("Введите год авто : ")
 
-brand_car = input("Введите бренд авто : ")
-model_car = input("Введите модель авто : ")
-year_car = input("Введите год авто : ")
+    cars = {
+        'Brand': 'Subaru',
+        'Model': 'Impreza',
+        'Year': '2005',
+        }
 
-cars = {
-    'Brand': 'Subaru',
-    'Model': 'Impreza',
-    'Year': '2005',
-}
+    cars['Year'] = year_car
+    cars['Model'] = model_car
+    cars['Brand'] = brand_car
 
-del_year = cars.popitem()
-del_model = cars.popitem()
-del_brand = cars.popitem()
-
-cars.setdefault(del_brand[0], brand_car)
-cars.setdefault(del_model[0], model_car)
-cars.setdefault(del_year[0], year_car)
-
-print(cars.items())
+    car.append(cars)
+print(car)
