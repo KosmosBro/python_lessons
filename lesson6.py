88d70a4a9109d5daa7d41b3bc416fe67bce60577
KosmosBro <76169732+KosmosBro@users.noreply.github.com>
2021-01-15 01:18:35 +0600

    Add files via upload

---
+++ lesson6.py
@@ -0,0 +1,15 @@
+from mymath import other
+import mymath as m
+import mymath.operations.arifmetic
+import mymath.operations.trigonometric as trig
+
+
+other.ro(11.62)
+
+print('___________''\n')
+
+m.operations.arifmetic.add(1, 4)
+
+print('___________''\n')
+
+trig.si(10, 12)
