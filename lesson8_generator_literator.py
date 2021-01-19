dd5868199b2b6a1f7646882e60087ba1a37206c5
KosmosBro <76169732+KosmosBro@users.noreply.github.com>
2021-01-19 14:54:51 +0600

    Update lesson8_generator_literator.py

--- lesson8_generator_literator.py
+++ lesson8_generator_literator.py
@@ -1,36 +1,22 @@
-# __ЗАДАНИЕ № 1 lambda__
 
 def number_t(x):  # Создаем фуннкцию number_t
-    return x + 100  # Возвращаем значение х и прибавляем к нему 100
+    return x + 10  # Возвращаем значение х и прибавляем к нему 100
 
 
 print(number_t((lambda x: x + 100)(200)))  # Вызываем функцию lambda
 
 print("__________________")
 
-# __ЗАДАНИЕ № 2 map__
 
 list_1 = range(1, 1000, 7)           # Создали список с элементами
 
 
-def ret(x):                     # Создали функцию с возвращением значения х
-    return x+20
-
-
-funk = list(map(ret, list_1))   # С помощью map спользовали фунцию ret для списка list_1
-print(funk)
-
+print(list(map(number_t, list_1)))      # С помощью map спользовали фунцию ret для списка list_1
 
 print("__________________")
 
-# __ЗАДАНИЕ № 3 iter()__
-
-spisok = {12, 23, 45, 67}
-
-
-kos = iter(spisok)          # Создали переменную kos и добавили в нее итератор
+kos = iter(list_1)          # Создали переменную kos и добавили в нее итератор
 print(next(kos))
 print(next(kos))
 print(next(kos))
 print(next(kos))
-
